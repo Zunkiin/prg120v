35516b3fc49bb3ec5b0d3d0a3cafb74e13025247
Zunkiin <zunkinx@gmail.com>
2025-09-17 14:03:55 +0200

    3

--- tema01/eksempler/eksempel-2.php
+++ tema01/eksempler/eksempel-2.php
@@ -8,7 +8,7 @@ $tall1=$_POST ["tall1"];
 $tall2=$_POST ["tall2"];
 
 $summen=$tall1 + $tall2;
-$differanse=$tall1 - $tall2;
+$differansen=$tall1 - $tall2;
 
 print ("Tall 1 er $tall1 <br />");
 print ("Tall 2 er $tall2 <br />");
