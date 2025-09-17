d7e72852a80a0f083c3ae2e4ff0244e2da55cd95
Zunkiin <zunkinx@gmail.com>
2025-09-17 13:48:34 +0200

    2

---
+++ tema01/eksempler/eksempel-2.php
@@ -0,0 +1,16 @@
+<?php /* Eksempel 2 */
+/*
+/*    Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
+/*    Programmet skriver ut de 2 tallene og summen og differansen av/mellom tallene
+*/
+
+$tall=$_POST ["tall1"];
+$tall=$_POST ["tall2"];
+
+$summen=$tall + $tall2;
+$differanse=$tall - $tall2;
+
+print ("Tall 1 er $tall <br />");
+print ("Tall 2 er $tall <br />");
+print ("Summen er $summen <br />");
+print ("Differansen er $differansen <br />");
