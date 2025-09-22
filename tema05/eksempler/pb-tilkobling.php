a9004fc51a2eab042155673887acaeab691cd894
Zunkiin <zunkinx@gmail.com>
2025-09-22 17:19:04 +0200

    tema 5

--- tema05/eksempler/pb-tilkobling.php
+++ tema05/eksempler/pb-tilkobling.php
@@ -2,11 +2,11 @@
 /*
 /*  Programmet foretar tilkobling til database-server og valg av database
 */
-$host = getenv('paber2152_HOST');
-$username = getenv('paber2152_USER');
-$password = getenv('paber2152_PASSWORD');
-$database = getenv('paber2152_DATABASE');
+$host = getenv('PB_HOST');
+$username = getenv('PB_USER');
+$password = getenv('PB_PASSWORD');
+$database = getenv('PB_DATABASE');
 
- $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+ $pb=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
     /* tilkobling til database-serveren utført */
  ?>
