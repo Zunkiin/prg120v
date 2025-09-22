8c59fdfe5e704638c53744ae5eff40caf0fce4f5
Zunkiin <zunkinx@gmail.com>
2025-09-22 18:36:12 +0200

    pb-tilkobling

--- tema05/eksempler/pb-tilkobling.php
+++ tema05/eksempler/pb-tilkobling.php
@@ -3,16 +3,16 @@
 /*
 /* Programmet foretar tilkobling til database-server og valg av database
 */
-$host = getenv('PB_HOST');
-$username = getenv('PB_USER');
-$password = getenv('PB_PASSWORD');
-$database = getenv('PB_DATABASE');
+$host = getenv('paber2152_HOST');
+$username = getenv('paber2152_USER');
+$password = getenv('paber2152_PASSWORD');
+$database = getenv('paber2152_DATABASE');
 
 // Force the connection to use TCP/IP by specifying the port
 $port = 3306; // Default MySQL/MariaDB port
 
 // The sixth parameter is the socket. Set it to null to force TCP/IP.
-$pb = mysqli_connect($host, $username, $password, $database, $port) or die("ikke kontakt med database-server");
+$paber2152 = mysqli_connect($host, $username, $password, $database, $port) or die("ikke kontakt med database-server");
 
 /* tilkobling til database-serveren utført */
 ?>
