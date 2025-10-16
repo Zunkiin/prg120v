4fe4fb20dfab8ffb9be43faf058c2713d2ab1999
Zunkiin <zunkinx@gmail.com>
2025-10-16 14:30:27 +0200

    vk

--- oppgave-2/1-vis-klasse.php
+++ oppgave-2/1-vis-klasse.php
@@ -1,8 +1,9 @@
 <?php
 /* vis-klasse.php
-   Programmet skriver ut alle registrerte klasser
+   Programmet skriver ut alle registrerte klasser fra databasen.
 */
-include("db-tilkobling.php");  // kobler til databasen
+
+include("db-tilkobling.php");  // Kobler til database-serveren og velger databasen
 ?>
 
 <!DOCTYPE html>
@@ -16,25 +17,36 @@ include("db-tilkobling.php");  // kobler til databasen
 <h3>Registrerte klasser</h3>
 
 <?php
+/* SQL spørring som henter alle klasser fra tabellen "klasse", sortert etter klassekode */
 $sqlSetning = "SELECT * FROM klasse ORDER BY klassekode;";
+
+/* Sender spørringen til databasen og lagrer resultatet */
 $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
 
+/* Teller hvor mange rader (klasser) som ble funnet */
 $antallRader = mysqli_num_rows($sqlResultat);
 
+/* Sjekker om databasen inneholder noen klasser */
 if ($antallRader == 0) {
   print("Ingen klasser er registrert i databasen.");
-} else {
+} 
+else {
+  /* Skriver ut overskrifter for tabellen */
   print("<table border='1'>");
   print("<tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>");
 
+  /* For løkke som henter en og en rad fra resultatet og skriver ut verdiene */
   for ($r = 1; $r <= $antallRader; $r++) {
-    $rad = mysqli_fetch_array($sqlResultat);
-    $klassekode = $rad["klassekode"];
-    $klassenavn = $rad["klassenavn"];
-    $studiumkode = $rad["studiumkode"];
+    $rad = mysqli_fetch_array($sqlResultat);   // Henter neste rad (én klasse)
+    $klassekode = $rad["klassekode"];          // Leser klassekode
+    $klassenavn = $rad["klassenavn"];          // Leser klassenavn
+    $studiumkode = $rad["studiumkode"];        // Leser studiumkode
 
+    /* Skriver ut en rad i HTML-tabellen */
     print("<tr><td>$klassekode</td><td>$klassenavn</td><td>$studiumkode</td></tr>");
   }
+
+  /* Avslutter HTML-tabellen */
   print("</table>");
 }
 ?>
